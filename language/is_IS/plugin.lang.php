34f73de6c6e629bf3c64eb1b41a653ab8af93bec
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-06-08 18:34:33 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Activate on'] = 'Virkjaðu þann';
+$lang['Moderate'] = 'Hóflegur';
+$lang['Noise color'] = 'Hávaðalitur';
+$lang['Noise level'] = 'Hávaðastyrkur';
+$lang['Nowhere'] = 'Hvergi';
+$lang['Only for unauthenticated users'] = 'Aðeins fyrir óstaðfesta notendur';
+$lang['Perturbation'] = 'Truflanir';
+$lang['Picture comments'] = 'Myndaumsagnir';
+$lang['Preview'] = 'Forskoðun';
+$lang['Random string'] = 'Handahófs strengur';
+$lang['Register form'] = 'Skráningareyðublað';
+$lang['Simple equation'] = 'Einföld jafna';
+$lang['Solve equation'] = 'Leystu jöfnu';
+$lang['Text color'] = 'Textalitur';
+$lang['Tip: type "random" on a color field to have a random color'] = 'Ábending: skrifaðu "handahóf" á litareit til að hafa handahófskenndan lit';
+$lang['We detected that EasyCaptcha plugin is available on your gallery. Both plugins can be used at the same time, but you should not under any circumstances activate both of them on the same page.'] = 'Við komumst að því að EasyCaptcha viðbótin er fáanleg í myndasafninu þínu. Hægt er að nota báðar viðbæturnar á sama tíma, en þú ættir ekki undir neinum kringumstæðum að virkja þær báðar á sömu síðu.';
+$lang['good value:'] = 'gott gildi:';
+$lang['random'] = 'handahóf';
+$lang['range:'] = 'svið:';
+$lang['Album comments'] = 'Umsagnir albúms';
+$lang['Background'] = 'Bakgrunnur';
+$lang['Background color'] = 'Bakgrunnslitur';
+$lang['Background image'] = 'Bakgrunnsmynd';
+$lang['Button color'] = 'Hnappalitur';
+$lang['Captcha theme'] = 'Captcha þema';
+$lang['Captcha type'] = 'Captcha tegund';
+$lang['Case sensitive'] = 'Hástafaviðkvæmt';
+$lang['Code lenght'] = 'Lengd kóða';
+$lang['Color'] = 'Litur';
+$lang['Comments action'] = 'Aðgerðir umsagna';
+$lang['Contact form'] = 'Hafa samband eyðublað';
+$lang['Enter code'] = 'Slá inn kóða';
+$lang['Font'] = 'Leturgerð';
+$lang['Guestbook'] = 'Gestabók';
+$lang['Image'] = 'Mynd';
+$lang['Invalid Captcha'] = 'Ógilt Captcha';
+$lang['Lines color'] = 'Línulitur';
+$lang['Lines density'] = 'Línuþykkt';
