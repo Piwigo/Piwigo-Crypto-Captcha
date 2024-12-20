e1b856ab4e121cb5ccefd1ba13d761796f5d8a0e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 18:06:17 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Activate on'] = 'הפעל ב';
+$lang['Album comments'] = 'תגובות אלבום';
+$lang['Background'] = 'רקע';
+$lang['Background color'] = 'צבע רקע';
+$lang['Background image'] = 'תמונת רקע';
+$lang['Button color'] = 'צבע לחצן';
+$lang['Captcha theme'] = 'ערכת Captcha';
+$lang['Captcha type'] = 'סוג Captcha';
+$lang['Case sensitive'] = 'רגיש רישיות';
+$lang['Code lenght'] = 'אורך קוד';
+$lang['Color'] = 'צבע';
+$lang['Comments action'] = 'פעולת תגובות';
+$lang['Contact form'] = 'טופס יצירת קשר';
+$lang['Enter code'] = 'הזן קוד';
+$lang['Font'] = 'גופן';
+$lang['Guestbook'] = 'ספר האורחים';
+$lang['Image'] = 'תמונה';
+$lang['Invalid Captcha'] = 'Captcha לא תקין';
+$lang['Lines color'] = 'צבע קווים';
+$lang['Lines density'] = 'צפיפות קווים';
+$lang['Moderate'] = 'להגביל';
+$lang['Noise color'] = 'צבע רעש';
+$lang['Noise level'] = 'רמת רעש';
+$lang['Nowhere'] = 'לאן שהיא';
+$lang['Only for unauthenticated users'] = 'רק למשתמשים לא מאומתים';
+$lang['Perturbation'] = 'הפרעה';
+$lang['Picture comments'] = 'תגובות תמונה';
+$lang['Preview'] = 'תצוגה מקדימה';
+$lang['Random string'] = 'מחרוזת אקראית';
+$lang['Register form'] = 'טופס הרשמה';
+$lang['Simple equation'] = 'משוואה פשוטה';
+$lang['Solve equation'] = 'פתור משוואה';
+$lang['Text color'] = 'צבע טקסט';
+$lang['Tip: type "random" on a color field to have a random color'] = 'טיפ: הקלד "אקראי" בשדה צבע כדי לקבל צבע אקראי';
+$lang['We detected that EasyCaptcha plugin is available on your gallery. Both plugins can be used at the same time, but you should not under any circumstances activate both of them on the same page.'] = 'זיהינו כי תוסף EasyCaptcha זמין בגלרייתך. ניתן להשתמש בשני התוספים בו זמנית, אך לא ניתן להפעיל שניהם באותו עמוד בשום תנאי.';
+$lang['good value:'] = 'ערך טוב:';
+$lang['random'] = 'אקראי';
+$lang['range:'] = 'טווח:';
